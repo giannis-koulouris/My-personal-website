2fe22f3d8f7e6d654d903908502a70dd9850333a
Giannis Koulouris <gianniskoulouris@outlook.com>
2018-06-27 23:30:31 +0300

    js changes

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
 
     $(".scroll-top").click(function() {
         $('html, body').animate({
-          scrollTop: 0
+            scrollTop: 0
         }, 800);
         return false;
       });
